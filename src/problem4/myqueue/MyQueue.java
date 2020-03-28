b746ab90ce452b7019e8f068ab711cdb6dd7ab21
Dharmendrasharma5583 <dharmendra.sharma_cs18@gla.ac.in>
2020-03-28 20:16:01 +0530

    Final Commit

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,47 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
- */
 package problem4.myqueue;
-// to create queue to store pre - order successor
 
-public class MyQueue {
+
+public class Node {
+
+    private int data;
+
+    private Node next;
+
+
+    public Node(int data) {
+
+        this.data = data;
+
+        next = null;
+
+    }
+
+
+    public int getData() {
+
+        return data;
+
+    }
+
+
+    public void setData(int data) {
+
+        this.data = data;
+
+    }
+
+
+    public Node getNext() {
+
+        return next;
+
+    }
+
+
+    public void setNext(Node next) {
+
+        this.next = next;
+
+    }
+
 }
